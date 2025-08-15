7931fe2b66d6dcb6ab9cb5765deb3735195c36f2
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-08-15 12:12:26 +0100

    InitialCommit

---
+++ add/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def solve(request):
+    value1 = int(request.GET['num1'])
+    value2 = int(request.GET['num2'])
+    op = request.GET['op']
+    if op == "+":
+        res = value1 + value2
+    elif op == "-":
+        res = value1 - value2
+    elif op == "*":
+        res = value1 * value2
+    elif op == "/":
+        res = value1 / value2
+
+    return render(request, "result.html", {
+        "result" : res
+    })
